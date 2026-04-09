6d3de3fb8d66dde77f7bf7f7429ac42312c02627
Adetunji Okekunle <tjokekunle@gmail.com>
2026-04-09 17:47:20 +0100

    beginner map exercises

--- script1.js
+++ script1.js
@@ -1,5 +1,30 @@
-const array=[1,2,3,4,5,6];
+const nums = [1, 2, 3, 4];
 
-const result = array.map((item)=> item*item);
+const double = nums.map((item)=>item*2);
 
-console.log(result);
+console.log(double);
+
+const names = ["alice", "bob", "charlie"];
+
+const upper = names.map((name)=>{
+    if (true) {return name.toUpperCase()}
+});
+console.log(upper);
+
+const items = ["apple", "banana", "cherry"];
+
+const addfruit = items.map(item=>(item+" fruit"));
+
+console.log(addfruit);
+
+
+const users = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 35 }
+];
+
+const justNames = users.map((named)=> {
+    return named[1]});
+
+console.log(justNames);
