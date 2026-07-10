5b9cf9ceb523c8341836f1eddf2d1c9f9cf20b85
Adetunji Okekunle <tjokekunle@gmail.com>
2026-07-10 19:00:03 +0100

    next is to make the JS capable of getting and refelcting data

--- test.js
+++ test.js
@@ -18,24 +18,46 @@ function addBookToLibrary(title, author, pages, readStatus) {
 
 function bookDisplay(library) {
   for (const book of library) {
+    const bookDiv = document.createElement("div");
     const p = document.createElement("p");
-    const deleteBtn= document.createElement("button");
+    const deleteBtn = document.createElement("button");
     const read = document.createElement("button");
-    p.append(`Book: ${book.title}, by ${book.author}, has ${book.pages} pages. Have I read it? ${book.readStatus}`);
-    p.append(deleteBtn);
-    p.append(read);
 
-    bookDisplayArea.append(p);
+    deleteBtn.setAttribute("class", "besideBook delete-btn");
+    read.setAttribute("class", "besideBook toggle-btn");
+    bookDiv.setAttribute("class", "book-card");
+    
+
+
+
+    deleteBtn.textContent = "Delete";
+    read.textContent = "toggle read";
+
+    p.append(`Book: ${book.title}, by ${book.author}, has ${book.pages} pages. Have I read it? ${book.readStatus}`);
+
+    bookDiv.append(p,deleteBtn,read);
+
+
+    bookDisplayArea.append(bookDiv);
   }
 }
 
-addBookToLibrary("book1","author1",900,"read");
+addBookToLibrary("book1", "author1", 900, "read");
+addBookToLibrary("book2", "author2", 800, "notread");
+addBookToLibrary("book3", "author3", 700, "read");
+
+
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
+
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
-bookDisplay(myLibrary);
 
+
+bookDisplay(myLibrary);
+form.addEventListener("submit", e => e.preventDefault())
 
