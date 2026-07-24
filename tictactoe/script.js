ec6610ee50fecd25b6a2e4f213e23140b39ddde3
AdeTME <tjokekunle@gmail.com>
2026-07-24 17:20:36 +0100

    can now generate the board on console, add player values, and restrict writing over already written cells

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,18 +1,54 @@
-function gameboard(){
+function gameboard() {
     const rows = 3;
     const columns = 3;
-    const cells =[];
+    const grid = [];
 
     for (let i = 0; i < rows; i++) {
-    cells[i] = [];
-    for (let j = 0; j < columns; j++) {
-      cells[i].push(Cell());
+        grid[i] = [];
+        for (let j = 0; j < columns; j++) {
+            grid[i].push(Cell());
+        }
     }
-    }
-      const getBoard = () => cells;
+    const getBoard = () => grid;
 
-      const fillCell = (rows,column, player) => {
-        const availableCell = cells
-            .filter((row)=>)
-  }
+    const dropToken = (row, column, player) => {
+        //check that the grip position isnt full and then drop the
+        //players mark where they so choose
+        if ((grid[row][column].getValue() === "")) {
+            grid[row][column].markCell(player);
+        }
+    };
+
+    const printGrid = () => {
+        const boardWithCellValues = grid.map((row) =>
+            row.map((cell) => cell.getValue())
+        );
+        console.log(boardWithCellValues);
+    };
+
+
+
+    return { getBoard, dropToken, printGrid }
 }
+
+function Cell() {
+
+    let value = "";
+
+    const markCell = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        markCell,
+        getValue,
+    };
+
+}
+const board = gameboard();
+board.dropToken(0, 0, "X");
+board.printGrid();
+board.dropToken(0, 0, "O"); // try overwriting the same cell
+board.printGrid(); // did it actually get blocked?
